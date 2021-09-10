0fe8e89f38755458054b0ca1c140e441bd2d75cd
Antonio Quintanilla <the64man1@yahoo.com>
2021-09-10 17:30:15 -0500

    Create arrayStringsAreEqual.js

---
+++ arrayStringsAreEqual.js
@@ -0,0 +1,11 @@
+/**
+ * Given two string arrays word1 and word2, return true if the two arrays represent the same string, and false otherwise.
+ * A string is represented by an array if the array elements concatenated in order forms the string.
+ * @param {string[]} word1
+ * @param {string[]} word2
+ * @return {boolean}
+ * word1[i] and word2[i] consist of lowercase letters.
+ */
+var arrayStringsAreEqual = function(word1, word2) {
+    return (word1.join('') === word2.join('')) ? true : false;
+};

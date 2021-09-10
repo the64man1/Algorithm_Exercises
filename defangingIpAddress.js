9f54896812dbd73d01df7e9b8e80f8838a3f03fc
maxquintanilla <the64man1@yahoo.com>
2021-09-10 11:44:39 -0500

    defang ip address

---
+++ defangingIpAddress.js
@@ -0,0 +1,10 @@
+/**
+ * Given a valid (IPv4) IP address, return a defanged version of that IP address
+ * A defanged IP address replaces every period "." with "[.]".
+ * @param {string} address
+ * @return {string}
+ */
+ var defangIPaddr = function(address) {
+    const regex = /\./g;
+    return address.replace(regex, '[.]');
+};

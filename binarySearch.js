6cefbb13ae8d2ec3c1ad0aa6d5b5127635a924c5
Antonio Quintanilla <the64man1@yahoo.com>
2021-09-17 11:12:20 -0500

    Create binarySearch.js

---
+++ binarySearch.js
@@ -0,0 +1,18 @@
+//Write a function called binarySearch which accepts a sorted array and a value and returns the index at which the value exists; otherwise, return -1;
+
+function binarySearch(arr, target){
+  let left = 0;
+  let right = arr.length - 1;
+  
+  while (left < right) {
+  	let middle = Math.ceil((left + right)/2);
+  	if (arr[middle] === target) {
+  	    return middle;
+  	} else if (arr[middle] > target) {
+  	    right--;
+  	} else {
+  	    left++;
+  	}
+  }
+  return -1;
+}

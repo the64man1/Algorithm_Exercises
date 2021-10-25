519e968ee220c9a626a94cf2f543a8d56c9090f2
Antonio Quintanilla <the64man1@yahoo.com>
2021-10-25 13:46:30 -0500

    Create middleOfLinkedList.js

---
+++ middleOfLinkedList.js
@@ -0,0 +1,28 @@
+/**
+ * Given the head of a singly linked list, return the middle node of the linked list.
+ * If there are two middle nodes, return the second middle node.
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var middleNode = function(head) {
+    let lengthCount = 0;
+    let headCopy = head;
+    while (headCopy) {
+        lengthCount++;
+        headCopy = headCopy.next;
+    }
+    let halfLen = Math.floor((lengthCount/2));
+    //console.log(halfLen)
+    while (halfLen > 0) {
+        halfLen--;
+        head = head.next;
+    }
+    return head
+};

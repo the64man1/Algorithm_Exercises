67738557f339474c77066f605f4ea055e2dfeaf8
Antonio Quintanilla <the64man1@yahoo.com>
2021-10-26 10:03:57 -0500

    Update longestCommonPrefix.js

--- longestCommonPrefix.js
+++ longestCommonPrefix.js
@@ -1,23 +1,23 @@
 /**
- * Write a function to find the longest common prefix string amongst an array of strings. If there is no common prefix, return an empty string "".
+ * Write a function to find the longest common prefix string amongst an array of strings.
+ * If there is no common prefix, return an empty string "".
  * @param {string[]} strs
  * @return {string}
  */
 var longestCommonPrefix = function(strs) {
-    if (strs.length === 1) return strs[0];
-    const sortStrs = strs.sort((a,b) => a.length - b.length)
-    if (sortStrs[0] === "") return "";
-    const firstStrSplit = sortStrs[0].split('');
-    let arr = [];
-    for (let i = 1; i < firstStrSplit.length + 1; i++) {
-        let head = firstStrSplit.slice(0, i).join('');
-        arr.push(head);
-        for (let j = 0; j < strs.length; j++) {
-            if (!strs[j].startsWith(head)) {
-                arr.pop();
-                return arr.length === 0 ? "" : arr.pop();
+    strs.sort((a,b) => a.length - b.length);
+    let shortestStr = strs[0].split('');
+    let longestSubStr = "";
+    for (let i = 1; i < shortestStr.length + 1; i++) {
+        let prefix = shortestStr.slice(0,i).join('');
+        for (let j = 1; j < strs.length; j++) {
+            if (!strs[j].startsWith(prefix)) {
+                prefix = prefix.split('');
+                prefix.pop();
+                prefix = prefix.join('');
+                return prefix;
             }
         }
     }
-    return sortStrs[0];
+    return shortestStr.join('');
 };
